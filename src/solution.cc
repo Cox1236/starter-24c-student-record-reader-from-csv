d3eeda8e5ebba8ebdd0eaf36e31854a7f0ec52bc
Cox1236 <yaoh4@illinois.edu>
2025-02-16 16:32:22 -0600

    Update solution.cc

--- src/solution.cc
+++ src/solution.cc
@@ -2,5 +2,14 @@
 
 Student ReadStudentRecFromStream(std::istream& is) {
   // your implementation here...
-  return Student{};
+  Student student = {};
+  std::string first_name;
+  std::string last_name;
+  unsigned int uin = 0;
+  unsigned double gpa = 0.0;
+  is >> first_name >> ' ' >> last_name >> ', ' >> uin >> ', ' >> gpa;
+  student.full_name = first_name + ' ' + last_name;
+  student.uin = uin;
+  student.gpa = gpa;
+  return student{student.full_name, student.uin, student.gpa};
 }
